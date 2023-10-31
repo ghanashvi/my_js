0d0219a7090b5a7ba8cdcec368ea856c2fbf78b0
Ghanashvi Parihar <100716990+ghanashvi@users.noreply.github.com>
2023-10-31 08:20:40 +0000

    day1

--- basics/basics1.js
+++ basics/basics1.js
@@ -1,1 +1,19 @@
+"use strict"; //treat all JS code as newer version
+//day 1
 console.log("Hello")
+
+//day2(variables)
+const accountId = 1445 //const -> cannot be changed
+// accountId =12 //not allowed
+
+let accountEmail = "raja@gmail.com" //modern practice defines scope
+let accountState;
+console.log(accountState); //print un defined
+
+var accountPassword = "12345" // avoid using can be redeclare and reinitailize and have global scope
+accountCity = "Bhopal" //variable can be declare in this way also but avoid using this
+
+console.table([accountId, accountEmail, accountPassword, accountCity]) //console method to print in tabular form
+
+
+
