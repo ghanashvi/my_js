384162592f897c79427271fc43fabdade20570db
Ghanashvi Parihar <100716990+ghanashvi@users.noreply.github.com>
2023-11-01 19:20:16 +0000

    numbers

--- basics/basics2.js
+++ basics/basics2.js
@@ -13,3 +13,33 @@ String Methods
 https://www.w3schools.com/js/js_string_methods.asp
 */
 
+//numbers and math
+const balance = new Number(100) //specifically cast --> prints [Number: 100]
+console.log(balance)
+//numbers method
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(balance.toFixed(2)) //precision value 100.00
+
+let num=123.89321
+console.log(num.toPrecision(3)) //takes precision but precision starts from left to right till asked value
+
+const hundereds=100000
+console.log(hundereds.toLocaleString()) //according to us standard
+console.log(hundereds.toLocaleString('en-IN')) //according to Indian standard
+
+//******************* Math ****************
+console.log(Math.abs(-4))
+console.log(Math.round(4.7))
+console.log(Math.ceil(4.6))
+console.log(Math.floor(4.3))
+console.log(Math.pow(12,2))
+console.log(Math.random) //always give value from 0-1
+
+console.log((Math.random()*10)+1)
+const min=10
+const max=20
+console.log(Math.floor(Math.random()+(max-min+1))+min) //imp formula
+
+//*************** Date and Time***********
+
