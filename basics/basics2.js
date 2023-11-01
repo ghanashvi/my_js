192b59ff5de38c35e806b8bc7b2086a6e066fa18
Ghanashvi Parihar <100716990+ghanashvi@users.noreply.github.com>
2023-11-01 05:39:15 +0000

    strings

---
+++ basics/basics2.js
@@ -0,0 +1,15 @@
+"use strict";
+//String  in JS
+//string interpolation is the process of inserting strings or values into an existing string for various purposes. => `hello ${variable_name}` => using backticks we can do various things in one go
+const name="Raja";
+const age=21;
+console.log(`hello my name is ${name} and my age is ${age}`)
+
+//string declaration using object
+const surname = new String("Ram")
+console.log(surname)
+/*
+String Methods
+https://www.w3schools.com/js/js_string_methods.asp
+*/
+
